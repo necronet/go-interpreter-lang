bdbd0a028cb0ac4af9b1111d9f7ee1147f099736
Jose Ayerdis <joseayerdis@Joses-MacBook-Pro-2.local>
2023-07-07 21:20:55 -0600

    Supports for additional tokens < > - !

--- token/token.go
+++ token/token.go
@@ -18,6 +18,13 @@ const(
 
 	ASSIGN = "="
 	PLUS = "+"
+	MINUS = "-"
+
+	BANG     = "!"
+        ASTERISK = "*"
+        SLASH    = "/"
+        LT = "<"
+        GT = ">"
 
 	COMMA = ","
 	SEMICOLON = ";"
