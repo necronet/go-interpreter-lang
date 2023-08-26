46580a971b9f3d7d099d37e2ae8e6bee313a4509
Jose Ayerdis <joseayerdis@gmail.com>
2023-08-26 17:55:39 -0600

    Worked on the return statement functionality

--- parser/parser.go
+++ parser/parser.go
@@ -1,7 +1,8 @@
 package parser
 
 
-import ( "necronet.info/interpreter/ast"
+import ("fmt" 
+		"necronet.info/interpreter/ast"
        "necronet.info/interpreter/lexer"
        "necronet.info/interpreter/token"
 )
@@ -11,10 +12,11 @@ type Parser struct {
 	l *lexer.Lexer
 	curToken  token.Token
 	peekToken token.Token
+	errors []string
 }
 
 func New(l *lexer.Lexer) *Parser { 
-	p := &Parser{l: l}
+	p := &Parser{l: l, errors: []string{},}
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
@@ -24,6 +26,15 @@ func New(l *lexer.Lexer) *Parser {
 func (p *Parser) nextToken() { 
 	p.curToken = p.peekToken 
 	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) Errors() []string { 
+	return p.errors 
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) ParseProgram() *ast.Program { 
@@ -46,6 +57,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 		case token.LET:	
 			return p.parseLetStatement() 
+		case token.RETURN:
+			return p.parseReturnStatement()
 		default:
 			return nil 
 	}
@@ -72,6 +85,18 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		return stmt
 }
 
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{ Token: p.curToken}
+
+	p.nextToken()
+
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
 func (p *Parser) curTokenIs(t token.TokenType) bool {
 	return p.curToken.Type == t
 }
@@ -86,6 +111,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true 
 	} else {
+		p.peekError(t)
 		return false 
 	}
 }
