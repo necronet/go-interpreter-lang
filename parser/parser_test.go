215cfa4c63b188a8b7b80b1f9f943e03f20ded60
Jose Ayerdis <joseayerdis@gmail.com>
2023-08-27 16:13:40 -0600

    init the pratt parser

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -113,4 +113,36 @@ func TestReturnStatements(t *testing.T) {
 		}
 	}
 }
+
+func TestIdentifierExpression(t *testing.T) {
+	input := "foobar;"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
 	
+	if len(program.Statements) != 1 {
+		t.Fatalf("Program has not enough statements. expected 1 got=%d", len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+
+	ident, ok := stmt.Expression.(*ast.Identifier)
+
+	if !ok {
+		t.Fatalf("exp not *ast.Identifier. got=%T", stmt.Expression)
+	}
+	if ident.Value != "foobar" {
+		t.Errorf("identValue not foobar. got=%s", ident.Value)
+	}
+
+	if ident.TokenLiteral() != "foobar" {
+		t.Errorf("ident.TokenLiteral not foobar. got=%s", ident.TokenLiteral())
+	}
+
+}
