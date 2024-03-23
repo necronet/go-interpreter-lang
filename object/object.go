38fad33d48ff72d40dc3c8c04ccf5c279abf8a70
Jose Ayerdis <joseayerdis@gmail.com>
2024-03-23 14:48:24 -0600

    Support for function and enclosed environments

--- object/object.go
+++ object/object.go
@@ -1,7 +1,10 @@
 package object
 
-import(
-    "fmt"
+import (
+	"bytes"
+	"fmt"
+    "strings"
+	"necronet.info/interpreter/ast"
 )
 
 type ObjectType string
@@ -12,6 +15,7 @@ const (
     NULL_OBJ = "NULL"
     RETURN_VALUE_OBJ = "RETURN_VALUE"
     ERROR_OBJ = "ERROR"
+    FUNCTION_OBJ = "FUNCTION"
 )
 
 type Object interface {
@@ -40,6 +44,28 @@ type Integer struct {
 func (i *Integer) Inspect() string {return fmt.Sprintf("%d", i.Value)}
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ } 
 
+type Function struct {
+    Parameters []*ast.Identifier
+    Body *ast.BlockStatement
+    Env *Environment
+}
+
+func (t *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+    var out bytes.Buffer
+    params := []string{}
+    for _, p := range f.Parameters {
+        params = append(params, p.String())
+    }
+
+    out.WriteString("fn")
+    out.WriteString("(")
+    out.WriteString(strings.Join(params, ","))
+    out.WriteString(") {\n")
+    out.WriteString(f.Body.String())
+    out.WriteString("\n")
+    return out.String()
+}
 
 type Boolean struct {
     Value bool
