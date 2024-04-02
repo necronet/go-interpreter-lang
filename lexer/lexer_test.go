2ea77865064cf2f1b2bfd4c9abd93e79af271388
Jose Ayerdis <joseayerdis@gmail.com>
2024-04-02 03:33:53 -0600

    support lexer colon preparing for hash parsing and evaluator

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -28,6 +28,7 @@ func TestNextToken(t *testing.T) {
      "foobar"
      "foo bar"
      [1, 2];
+     {"foo" : "bar"}
 	`
 
 	tests := []struct {
@@ -50,7 +51,6 @@ func TestNextToken(t *testing.T) {
         {token.IDENT, "add"},
         {token.ASSIGN, "="},
         {token.FUNCTION, "fn"},
-
 
         {token.LPAREN, "("},
         {token.IDENT, "x"},
@@ -120,6 +120,12 @@ func TestNextToken(t *testing.T) {
         {token.INT, "2" },
         {token.RBRACKET, "]" },
         {token.SEMICOLON, ";" },
+
+        {token.LBRACE, "{" },
+        {token.STRING, "foo" },
+        {token.COLON, ":" },
+        {token.STRING, "bar" },
+        {token.RBRACE, "}" },
         {token.EOF, ""},
     }
 
