0cdab3a7399f0abf9685efa6842da5a846fcf4a3
Jose Ayerdis <joseayerdis@gmail.com>
2024-04-03 01:14:15 -0600

    Support for stdout printing

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -2,6 +2,7 @@ package evaluator
 
 import (
     "necronet.info/interpreter/object"
+    "fmt"
 )
 
 var builtins = map[string]*object.Builtin{
@@ -95,6 +96,14 @@ var builtins = map[string]*object.Builtin{
             newElements[length] = args[1]
 
             return &object.Array{Elements: newElements }
+        },
+    },
+    "puts": &object.Builtin{
+        Fn: func(args ...object.Object) object.Object {
+            for _, arg := range args {
+                fmt.Println(arg.Inspect())
+            }
+            return NULL
         },
     },
 }
