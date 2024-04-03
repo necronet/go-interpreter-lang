86c86fb8603cd2d90140fd9865f7955b21e5409a
Jose Ayerdis <joseayerdis@gmail.com>
2024-04-03 00:28:33 -0600

    Support parsing the hash keys

--- ast/ast.go
+++ ast/ast.go
@@ -31,6 +31,28 @@ func (sl *StringLiteral) expressionNode() {}
 func (sl *StringLiteral) TokenLiteral() string { return sl.Token.Literal }
 func (sl *StringLiteral) String() string { return sl.Token.Literal }
 
+type HashLiteral struct {
+    Token token.Token
+    Pairs map[Expression]Expression
+}
+
+func (hl *HashLiteral) expressionNode() {}
+func (hl *HashLiteral) TokenLiteral() string { return hl.Token.Literal }
+func (hl *HashLiteral) String() string {
+
+    var out bytes.Buffer
+
+    pairs := []string{}
+    for key, value := range hl.Pairs {
+        pairs = append(pairs, key.String()+":"+value.String())
+    }
+    out.WriteString("{")
+    out.WriteString(strings.Join(pairs,","))
+    out.WriteString("}")
+    return out.String()
+
+}
+
 type Program struct {
 	Statements []Statement
 }
